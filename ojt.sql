694fb4e373f442a2548a286a6de3ec0868ab2596
renzzzdc <renzdelacruz0608@gmail.com>
2025-03-17 16:53:15 +0800

    Updated

--- ojt.sql
+++ ojt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Mar 11, 2025 at 09:26 AM
+-- Generation Time: Mar 17, 2025 at 09:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,72 +44,106 @@ CREATE TABLE `cto_requests` (
 
 INSERT INTO `cto_requests` (`request_id`, `employee_name`, `office`, `position`, `filing_date`, `earned_hours`, `cto_date`, `applied_hours`) VALUES
 (483, 'Chabelita', 'Office of the Governor (GO)', 'Intern', '2025-03-11', 5, '2025-03-11', 2),
-(484, 'user', 'Office of the Vice Governor - Personal Staff', 'user', '2025-03-11', 1, '2025-03-11', 1);
+(484, 'user', 'Office of the Vice Governor - Personal Staff', 'user', '2025-03-11', 1, '2025-03-11', 1),
+(485, '1', 'Office of the Secretary to the Sangguniang Panlalawigan (OSSP)', '1', '2025-03-12', 1, '2025-03-12', 1),
+(486, 'bb', 'Office of the Vice Governor (VGO)', 'bb', '2025-03-12', 1, '2025-03-12', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee_info`
+-- Table structure for table `employeedetails`
 --
 
-CREATE TABLE `employee_info` (
+CREATE TABLE `employeedetails` (
   `id` int(11) NOT NULL,
   `office` varchar(255) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) DEFAULT NULL,
+  `middle_name` varchar(100) NOT NULL,
   `filing_date` date NOT NULL,
-  `position` varchar(100) NOT NULL,
+  `position` varchar(255) NOT NULL,
   `salary` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `employeedetails`
+--
+
+INSERT INTO `employeedetails` (`id`, `office`, `last_name`, `first_name`, `middle_name`, `filing_date`, `position`, `salary`) VALUES
+(1, 'HR Department', 'Doe', 'John', 'A', '2025-03-10', 'HR Manager', 50000.00),
+(108, '4', 'Dela Cruz', 'renz', 'culalic', '2025-03-17', 'Intern', 0.00),
+(109, '20', 'Dela Cruz', 'Chabelita', 'Pita', '2025-03-17', 'Intern', 0.00),
+(110, '1', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00),
+(111, 'Office of the Governor (GO)', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00),
+(112, 'Office of the Governor - Personal Staff', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00),
+(113, 'Office of the Governor - Personal Staff', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00),
+(114, 'Office of the Governor - Personal Staff', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00),
+(115, 'Office of the Governor - Personal Staff', 'bb', 'bb', 'bb', '2025-03-17', 'bb', 0.00);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `leave_approval`
+-- Table structure for table `leaveapproval`
 --
 
-CREATE TABLE `leave_approval` (
+CREATE TABLE `leaveapproval` (
   `id` int(11) NOT NULL,
-  `employee_id` int(11) DEFAULT NULL,
+  `employee_id` int(11) NOT NULL,
+  `leave_id` int(11) NOT NULL,
   `as_of` date NOT NULL,
-  `vacation_total_earned` int(11) DEFAULT NULL,
-  `sick_total_earned` int(11) DEFAULT NULL,
-  `vacation_less_application` int(11) DEFAULT NULL,
-  `sick_less_application` int(11) DEFAULT NULL,
-  `vacation_balance` int(11) DEFAULT NULL,
-  `sick_balance` int(11) DEFAULT NULL,
-  `recommendation` varchar(255) DEFAULT NULL,
-  `approval_status` varchar(255) DEFAULT NULL,
-  `days_with_pay` int(11) DEFAULT NULL,
-  `days_without_pay` int(11) DEFAULT NULL,
-  `others_specify` varchar(255) DEFAULT NULL,
-  `disapproved_to` varchar(255) DEFAULT NULL
+  `vacation_leave_balance` decimal(10,2) NOT NULL,
+  `vacation_less_application` decimal(10,2) NOT NULL,
+  `sick_leave_balance` decimal(10,2) NOT NULL,
+  `sick_less_application` decimal(10,2) NOT NULL,
+  `days_with_pay` int(11) NOT NULL,
+  `days_without_pay` int(11) NOT NULL,
+  `disapproved_days` int(11) DEFAULT NULL,
+  `disapproved_reason` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `leaveapproval`
+--
+
+INSERT INTO `leaveapproval` (`id`, `employee_id`, `leave_id`, `as_of`, `vacation_leave_balance`, `vacation_less_application`, `sick_leave_balance`, `sick_less_application`, `days_with_pay`, `days_without_pay`, `disapproved_days`, `disapproved_reason`) VALUES
+(1, 1, 1, '2025-03-14', 10.00, 2.00, 8.00, 1.00, 5, 2, NULL, NULL),
+(84, 108, 108, '2025-03-17', 2.00, 1.00, 1.00, 2.00, 3, 3, NULL, NULL),
+(85, 109, 109, '2025-03-17', 0.00, 0.00, 1.00, 0.00, 0, 1, NULL, NULL),
+(86, 110, 110, '2025-03-17', 0.00, 0.00, 0.00, 0.00, 0, 0, NULL, NULL),
+(87, 115, 115, '2025-03-17', 0.00, 0.00, 0.00, 0.00, 0, 0, NULL, 'asdasd');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `leave_details`
+-- Table structure for table `leavedetails`
 --
 
-CREATE TABLE `leave_details` (
+CREATE TABLE `leavedetails` (
   `id` int(11) NOT NULL,
   `employee_id` int(11) DEFAULT NULL,
-  `type_of_leave` varchar(255) DEFAULT NULL,
-  `vacation_within_philippines` varchar(255) DEFAULT NULL,
-  `vacation_abroad` varchar(255) DEFAULT NULL,
-  `sick_hospital` varchar(255) DEFAULT NULL,
-  `sick_out_patient` varchar(255) DEFAULT NULL,
-  `special_leave_women` varchar(255) DEFAULT NULL,
-  `study_leave_master` tinyint(1) DEFAULT NULL,
-  `study_leave_exam` tinyint(1) DEFAULT NULL,
-  `monetization` tinyint(1) DEFAULT NULL,
-  `terminal_leave` tinyint(1) DEFAULT NULL,
+  `leave_type` enum('sickLeave','forceLeave','vacationLeave','maternityLeave','paternityLeave','specialPrivilege','studyLeave','vawcLeave','rehabPriv','specialLeave','specialEmergency','others') NOT NULL,
+  `leave_type_others` varchar(255) DEFAULT NULL,
+  `detail_type` enum('withinPhilippines','abroad','hospital','outPatient','leaveWomen','completion','exam','monetization','terminal') DEFAULT NULL,
+  `detail_description` varchar(255) DEFAULT NULL,
   `working_days` int(11) NOT NULL,
   `inclusive_dates` date NOT NULL,
-  `commutation` varchar(50) DEFAULT NULL
+  `commutation` enum('notRequested','requested') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leavedetails`
+--
+
+INSERT INTO `leavedetails` (`id`, `employee_id`, `leave_type`, `leave_type_others`, `detail_type`, `detail_description`, `working_days`, `inclusive_dates`, `commutation`) VALUES
+(1, 1, 'sickLeave', NULL, 'hospital', 'Flu', 5, '2025-03-10', 'requested'),
+(108, 108, 'vacationLeave', NULL, 'withinPhilippines', 'Siargao', 12, '2025-03-17', 'requested'),
+(109, 109, 'sickLeave', NULL, 'hospital', 'Lagnat', 3, '2025-03-17', 'requested'),
+(110, 110, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'notRequested'),
+(111, 111, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'notRequested'),
+(112, 112, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'notRequested'),
+(113, 113, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'notRequested'),
+(114, 114, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'notRequested'),
+(115, 115, 'vacationLeave', NULL, 'withinPhilippines', 'bb', 0, '2025-03-17', 'requested');
 
 -- --------------------------------------------------------
 
@@ -146,7 +180,16 @@ INSERT INTO `locator_slip` (`id`, `official`, `date`, `destination`, `purpose`, 
 (16, 1, '2025-03-04', 'Malolos', 'Tambay', '2025-03-10', '09:37 AM', '12:36 PM', 'user3\r\n'),
 (17, 1, '0000-00-00', 'Malolos', 'Tambay', '', '01:00 AM', '01:00 AM', 'Chabelita'),
 (18, 0, '2024-02-28', 'Provincial Office', 'Submit Reports', '2024-02-28', '08:30 AM', '02:30 PM', 'Robert White'),
-(19, 1, '0000-00-00', 'Plaridel', 'Wala', '', '01:00 AM', '01:00 AM', 'Marie');
+(19, 1, '0000-00-00', 'Plaridel', 'Wala', '', '01:00 AM', '01:00 AM', 'Marie'),
+(26, 1, '2025-03-12', 'dd', 'dd', '2025-03-12', '09:16 AM', '09:19 AM', 'dd'),
+(27, 1, '2025-03-12', 'cc', 'cc', '2025-03-12', '09:19 AM', '09:19 AM', 'cc'),
+(28, 0, '2025-03-01', 'Location', 'Agenda', '2025-03-01', '06:19 PM', '09:19 PM', 'User'),
+(29, 1, '2025-03-12', 'aaaaa', 'aaaaaaa', '2025-03-12', '09:25 AM', '09:25 AM', 'aaaaaaa'),
+(30, 1, '2025-03-12', '2', '2', '2025-03-27', '02:22 PM', '10:23 AM', '2'),
+(31, 0, '2025-03-12', '1', '1', '2025-03-12', '02:01 PM', '02:01 AM', '1'),
+(32, 1, '2025-03-12', 'Malolos', 'Tambay', '2025-03-12', '02:19 PM', '02:19 PM', 'Marie'),
+(33, 1, '2025-03-12', 'Malolos', 'Tambay', '2025-03-12', '02:19 PM', '02:19 PM', 'Marie'),
+(34, 1, '2025-03-25', 'aaaaaa', 'Tambay', '2025-03-06', '03:12 AM', '03:12 AM', 'Chabelita');
 
 -- --------------------------------------------------------
 
@@ -226,22 +269,23 @@ ALTER TABLE `cto_requests`
   ADD PRIMARY KEY (`request_id`);
 
 --
--- Indexes for table `employee_info`
+-- Indexes for table `employeedetails`
 --
-ALTER TABLE `employee_info`
+ALTER TABLE `employeedetails`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `leave_approval`
+-- Indexes for table `leaveapproval`
 --
-ALTER TABLE `leave_approval`
+ALTER TABLE `leaveapproval`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `employee_id` (`employee_id`);
+  ADD KEY `employee_id` (`employee_id`),
+  ADD KEY `leave_id` (`leave_id`);
 
 --
--- Indexes for table `leave_details`
+-- Indexes for table `leavedetails`
 --
-ALTER TABLE `leave_details`
+ALTER TABLE `leavedetails`
   ADD PRIMARY KEY (`id`),
   ADD KEY `employee_id` (`employee_id`);
 
@@ -271,31 +315,31 @@ ALTER TABLE `remarks`
 -- AUTO_INCREMENT for table `cto_requests`
 --
 ALTER TABLE `cto_requests`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=485;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=487;
 
 --
--- AUTO_INCREMENT for table `employee_info`
+-- AUTO_INCREMENT for table `employeedetails`
 --
-ALTER TABLE `employee_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=605;
+ALTER TABLE `employeedetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 
 --
--- AUTO_INCREMENT for table `leave_approval`
+-- AUTO_INCREMENT for table `leaveapproval`
 --
-ALTER TABLE `leave_approval`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `leaveapproval`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
--- AUTO_INCREMENT for table `leave_details`
+-- AUTO_INCREMENT for table `leavedetails`
 --
-ALTER TABLE `leave_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `leavedetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 
 --
 -- AUTO_INCREMENT for table `locator_slip`
 --
 ALTER TABLE `locator_slip`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `offices`
@@ -314,16 +358,17 @@ ALTER TABLE `remarks`
 --
 
 --
--- Constraints for table `leave_approval`
+-- Constraints for table `leaveapproval`
 --
-ALTER TABLE `leave_approval`
-  ADD CONSTRAINT `leave_approval_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee_info` (`id`) ON DELETE CASCADE;
+ALTER TABLE `leaveapproval`
+  ADD CONSTRAINT `leaveapproval_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employeedetails` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `leaveapproval_ibfk_2` FOREIGN KEY (`leave_id`) REFERENCES `leavedetails` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `leave_details`
+-- Constraints for table `leavedetails`
 --
-ALTER TABLE `leave_details`
-  ADD CONSTRAINT `leave_details_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee_info` (`id`) ON DELETE CASCADE;
+ALTER TABLE `leavedetails`
+  ADD CONSTRAINT `leavedetails_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employeedetails` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
